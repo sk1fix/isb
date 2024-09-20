f6c9c3a75b88b980d16afdb0a03879786362df24
sk1fix <agafonovtoha427@gmail.com>
2024-09-20 17:07:01 +0400

    functions

---
+++ lab_3/functions.py
@@ -0,0 +1,46 @@
+import json
+
+
+class Func:
+    """
+    A class for working with files, reading data from them and writing
+    """
+    def read_json(path: str) -> dict:
+        '''
+            The function reads the key from the json file
+        Args:
+            path (str): path to file
+        '''
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print('No such file or directory')
+
+
+
+    def read_txt(path: str) -> dict:
+        '''
+            The function reads the key from the json file
+        Args:
+            path (str): path to file
+        '''
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                return file.read()
+        except FileNotFoundError:
+            print('No such file or directory')
+
+
+    def write_to_file(path: str, text: str) -> None:
+        """function that can write data to .txt file
+
+        Args:
+            path (str): path to file
+            text (str): what we need to write in file
+        """
+        try:
+            with open(path, 'a', encoding='utf-8') as file:
+                file.write(text)
+        except IOError:
+            print('All bad')
